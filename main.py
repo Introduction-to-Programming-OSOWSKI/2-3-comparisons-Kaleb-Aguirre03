b2883d3b9c01c22b794bb50ac04344c87eb4c694
Kaleb-Aguirre03 <820930@mystma.org>
2021-09-28 09:43:16 -0500

    #Not Finished Yet

--- main.py
+++ main.py
@@ -1,1 +1,50 @@
-#WRITE YOUR CODE IN THIS FILE
+#Define the Function greaterThan()
+def greaterThan(x, y):
+    if x > y:
+        return True
+
+    else:
+        return False
+
+#Define the Function lessThan()
+def lessThan(x, y):
+    if x < y:
+        return True
+
+    else:
+        return False
+
+#Define the Function equalTo()
+def equalTo(x, y):
+    if x == y:
+        return True
+
+    else:
+        return False
+
+#Define the Function greaterOrEqual()
+def greaterOrEqual(x, y):
+    if x >= y:
+        return True
+
+    else:
+        return False
+
+#Define the Function lessOrEqual()
+def lessOrEqual(x,y):
+    if x <= y:
+        return True
+
+    else:
+        return False
+
+
+#Test each Function with the print command
+
+print (greaterThan(10, 5))
+print (greaterThan(5, 10))
+
+print (lessThan(10, 5))
+print (lessThan(5, 10))
+
+print (equalTo)
